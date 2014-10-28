590f79ece4aac561179a66c3d098d0aceae6af8d
jcThePants <jcthepants@gmail.com>
2014-10-27 17:53:44 -0700

    Implement localization and fix existing localization to match changes

--- src/com/jcwhatever/bukkit/musical/Lang.java
+++ src/com/jcwhatever/bukkit/musical/Lang.java
@@ -24,16 +24,13 @@
 
 package com.jcwhatever.bukkit.musical;
 
-import com.jcwhatever.bukkit.generic.language.LanguageManager;
 import com.jcwhatever.bukkit.generic.language.Localized;
 
 public class Lang {
     private Lang() {}
 
-    private static LanguageManager _languageManager = new LanguageManager();
-
     @Localized
     public static String get(String text, Object... params) {
-        return _languageManager.get(MusicalRegions.getInstance(), text, params);
+        return MusicalRegions.getInstance().getLanguageManager().get(text, params);
     }
 }
